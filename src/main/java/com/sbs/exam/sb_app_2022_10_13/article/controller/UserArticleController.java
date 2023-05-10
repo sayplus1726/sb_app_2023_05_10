2a18c9ea4f00529f2c79d3a3d23cd71fb403410c
sayplus1726 <sayplus1726@naver.com>
2023-05-10 14:31:59 +0900

    ArticleService에 있던 단순 데이터 저장과 관련된 로직은 ArticleRepository로 옮김

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/article/controller/UserArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/article/controller/UserArticleController.java
@@ -7,62 +7,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class UserArticleController {
 
-  //인스턴스 변수 시작
   @Autowired
   private ArticleService articleService;
-  int articleLastId;
 
-  private List<Article> articles;
-  //인스턴스 변수 끝
-
-  //생성자
-  public UserArticleController() {
-    articles = new ArrayList<>();
-    articleLastId = 0;
-
-    makeTestData();
-  }
-
-  //서비스 메서드 시작
-  private void makeTestData() {
-    for(int i=1; i<=10; i++) {
-      String title = "제목" + i;
-      String body = "내용" + i;
-
-      writeArticle(title, body);
-    }
-  }
-
-  private Article getArticle(int id) {
-    for(Article article : articles) {
-      if ( article.getId() == id) {
-        return article;
-      }
-    }
-    return null;
-  }
-
-  private Article writeArticle(String title, String body) {
-    int id = articleLastId + 1;
-    Article article = new Article(id, title, body);
-
-    articles.add(article);
-    articleLastId = id;
-
-    return article;
-  }
-  //서비스 메서드 끝
-
+  //액션 메서드 시작
   @RequestMapping("/user/article/doAdd")
   @ResponseBody
   public Article doAdd(String title, String body) {
-    Article article = writeArticle(title, body);
+    Article article = articleService.writeArticle(title, body);
 
     return article;
   }
@@ -70,59 +27,48 @@ public class UserArticleController {
   @RequestMapping("/user/article/getArticles")
   @ResponseBody
   public List<Article> getArticles() {
-    return articles;
+    return articleService.getArticles();
   }
 
   @RequestMapping("/user/article/getArticle")
   @ResponseBody
   public Object getArticleAction(int id) {
-    Article article = getArticle(id);
+    Article article = articleService.getArticle(id);
 
-    if ( article == null) {
+    if (article == null) {
       return id + "번 게시물이 존재하지 않습니다.";
     }
+
     return article;
   }
 
   @RequestMapping("/user/article/doDelete")
   @ResponseBody
   public String doDelete(int id) {
-    Article article = getArticle(id);
+    Article article = articleService.getArticle(id);
 
-    if(article == null) {
+    if (article == null) {
       return id + "번 게시물이 존재하지 않습니다.";
     }
 
-    deleteArticle(id);
+    articleService.deleteArticle(id);
 
     return id + "번 게시물을 삭제하였습니다.";
-  }
-
-  private void deleteArticle(int id) {
-    Article article = getArticle(id);
-
-    articles.remove(article);
   }
 
   @RequestMapping("/user/article/doModify")
   @ResponseBody
   public String doModify(int id, String title, String body) {
-    Article article = getArticle(id);
 
-    if(article == null) {
+    Article article = articleService.getArticle(id);
+
+    if (article == null) {
       return id + "번 게시물이 존재하지 않습니다.";
     }
 
-    modifyArticle(id, title, body);
+    articleService.modifyArticle(id, title, body);
 
     return id + "번 게시물을 수정하였습니다.";
   }
-
-  private void modifyArticle(int id, String title, String body) {
-    Article article = getArticle(id);
-
-    article.setTitle(title);
-    article.setBody(body);
-  }
-
+  // 액션 메서드 끝
 }
--- src/main/java/com/sbs/exam/sb_app_2022_10_13/article/service/ArticleService.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/article/service/ArticleService.java
@@ -1,7 +1,38 @@
 package com.sbs.exam.sb_app_2022_10_13.article.service;
 
+import com.sbs.exam.sb_app_2022_10_13.article.repository.ArticleRepository;
+import com.sbs.exam.sb_app_2022_10_13.article.vo.Article;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ArticleService {
+
+  private ArticleRepository articleRepository;
+
+  public ArticleService(ArticleRepository articleRepository) {
+    this.articleRepository = articleRepository;
+    articleRepository.makeTestData();
+  }
+
+  public Article writeArticle(String title, String body) {
+    return articleRepository.writeArticle(title, body);
+  }
+
+  public List<Article> getArticles() {
+    return articleRepository.getArticles();
+  }
+
+  public Article getArticle(int id) {
+    return articleRepository.getArticle(id);
+  }
+
+  public void deleteArticle(int id) {
+    articleRepository.deleteArticle(id);
+  }
+
+  public void modifyArticle(int id, String title, String body) {
+    articleRepository.modifyArticle(id, title, body);
+  }
 }
