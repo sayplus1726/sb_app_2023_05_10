506ae08905faa60fa51ccf84c3c760a87559d042
sayplus1726 <sayplus1726@naver.com>
2023-05-11 14:37:10 +0900

    MemberRepository 추가, 회원가입 처리

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/member/service/MemberService.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/member/service/MemberService.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Service;
 public class MemberService {
     private MemberRepository memberRepository;
 
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
         memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
     }
