e78eb111abb1f9d834dc62694ef0b96b02651f4b
sayplus1726 <sayplus1726@naver.com>
2023-05-11 13:12:22 +0900

    게시물 수정시, 입력된 정보만 수정

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/article/repository/ArticleRepository.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/article/repository/ArticleRepository.java
@@ -9,24 +9,56 @@ import java.util.List;
 public interface ArticleRepository {
 
   // INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = ?, `body` = ?
-  @Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
-  public void writeArticle(String title, String body);
+  @Insert("""
+          INSERT INTO article
+          SET regDate = NOW(),
+          updateDate = NOW(),
+          title = #{title},
+          `body` = #{body}"
+          """)
+  public void writeArticle(@Param("title") String title, @Param("body") String body);
 
   // SELECT * FROM article WHERE id = ?
-  @Select("SELECT * FROM article WHERE id = #{id}")
-  public Article getArticle(int id);
+  @Select("""
+          SELECT *
+          FROM article
+          WHERE id = #{id}
+          """)
+  public Article getArticle(@Param("id") int id);
 
   // DELETE FROM article WHERE id = ?
-  @Delete("DELETE FROM article WHERE id = #{id}")
+  @Delete("""
+          DELETE
+          FROM article
+          WHERE id = #{id}
+          """)
   public void deleteArticle(int id);
 
   // SELECT * FROM article ORDER BY id DESC;
- @Select("SELECT * FROM article ORDER BY id DESC")
+ @Select("""
+         SELECT *
+         FROM article
+         ORDER BY id DESC
+         """)
   public List<Article> getArticles();
 
   // UPDATE article SET title = ?, `body` = ?, updateDate = NOW() WHERE id = ?
-  @Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
-  public void modifyArticle(int id, String title, String body);
+  @Update("""
+          <script>
+          UPDATE article
+          <set>
+            <if test='title != null'>
+             title = #{title},
+            </if>
+            <if test='body != null'>
+             `body` = #{body},
+            </if>
+           updateDate = NOW()
+          </set>
+          WHERE id = #{id}
+          </script>
+          """)
+  public void modifyArticle(@Param("id") int id, @Param("title") String title, @Param("body") String body);
 
   @Select("SELECT LAST_INSERT_ID()")
   public int getLastInsertId();
