e2dc78192b286ab0a1a766f20aec7e373e491307
sayplus1726 <sayplus1726@naver.com>
2023-05-11 10:51:47 +0900

    INSERT 쿼리를 메서드와 연결, last_insert_id 사용

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/article/repository/ArticleRepository.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/article/repository/ArticleRepository.java
@@ -1,10 +1,7 @@
 package com.sbs.exam.sb_app_2022_10_13.article.repository;
 
 import com.sbs.exam.sb_app_2022_10_13.article.vo.Article;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -12,7 +9,8 @@ import java.util.List;
 public interface ArticleRepository {
 
   // INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = ?, `body` = ?
-  public Article writeArticle(String title, String body);
+  @Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+  public void writeArticle(String title, String body);
 
   // SELECT * FROM article WHERE id = ?
   @Select("SELECT * FROM article WHERE id = #{id}")
@@ -30,4 +28,6 @@ public interface ArticleRepository {
   @Update("UPDATE article SET title = #{title}, `body` = #{body}, updateDate = NOW() WHERE id = #{id}")
   public void modifyArticle(int id, String title, String body);
 
+  @Select("SELECT LAST_INSERT_ID()")
+  public int getLastInsertId();
 }
