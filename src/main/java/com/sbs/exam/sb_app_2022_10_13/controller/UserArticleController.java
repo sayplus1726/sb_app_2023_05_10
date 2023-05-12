63391a8717ff700c4f67f61c6b40b44dda7551ff
sayplus1726 <sayplus1726@naver.com>
2023-05-12 11:25:19 +0900

    C:/Program Files/Git/user/article/doAdd에 ResultData.newData 적용

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UserArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UserArticleController.java
@@ -20,7 +20,7 @@ public class UserArticleController {
   //액션 메서드 시작
   @RequestMapping("/user/article/doAdd")
   @ResponseBody
-  public ResultData doAdd(String title, String body) {
+  public ResultData<Article> doAdd(String title, String body) {
     if(Ut.empty(title)) {
       return ResultData.from("F-1", "title을 입력해주세요.");
     }
@@ -29,12 +29,14 @@ public class UserArticleController {
       return ResultData.from("F-2", "body를 입력해주세요.");
     }
 
+    //성공시 S-1
+    //
     ResultData<Integer> writeArticleRd = articleService.writeArticle(title, body);
 
     int id = writeArticleRd.getData1();
     Article article = articleService.getArticle(id);
 
-    return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), article);
+    return ResultData.newData(writeArticleRd, article);
   }
 
   @RequestMapping("/user/article/getArticles")
