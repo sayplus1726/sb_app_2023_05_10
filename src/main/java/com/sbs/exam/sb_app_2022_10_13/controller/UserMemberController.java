f085b0e9e7730beb5ed039ea683744fb1b71c4b5
sayplus1726 <sayplus1726@naver.com>
2023-05-12 15:30:59 +0900

    로그아웃 기능 구현

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UserMemberController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UserMemberController.java
@@ -95,4 +95,22 @@ public class UserMemberController {
     return ResultData.from("S-1", Ut.f("%s님 환영합니다.", member.getNickname()));
   }
 
+  @RequestMapping("/user/member/doLogout")
+  @ResponseBody
+  public ResultData doLogout(HttpSession httpSession) {
+    boolean isLogined = false;
+
+    if (httpSession.getAttribute("loginedMemberId") == null) {
+      isLogined = true;
+    }
+
+    if (isLogined) {
+      return ResultData.from("S-1", "이미 로그아웃 상태입니다.");
+    }
+
+    httpSession.removeAttribute("loginedMemberId");
+
+    return ResultData.from("S-2", "로그아웃 되었습니다.");
+  }
+
 }
