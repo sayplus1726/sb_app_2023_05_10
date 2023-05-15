784a5e4fd1ff1836c6713ae476a7c0694bc38f5b
sayplus1726 <sayplus1726@naver.com>
2023-05-15 10:29:52 +0900

    JSP 적용

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UserHomeController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UserHomeController.java
@@ -2,14 +2,17 @@ package com.sbs.exam.sb_app_2022_10_13.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserHomeController {
   @RequestMapping("/user/home/main")
-  @ResponseBody
   public String main() {
-    return "안녕";
+    return "user/home/main";
+  }
+
+  @RequestMapping
+  public String showRoot() {
+    return "redirect:/user/home/main";
   }
 
 }
---
+++ src/main/webapp/WEB-INF/jsp/user/home/main.jsp
@@ -0,0 +1,17 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>메인</title>
+</head>
+<body>
+    <h1>안녕하세요.</h1>
+    <div>
+        123
+    </div>
+    <div>1234</div>
+</body>
+</html>
