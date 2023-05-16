e0af0c861242138e32e4857b337c34f5416431aa
sayplus1726 <sayplus1726@naver.com>
2023-05-16 09:20:00 +0900

    로그인 폼 구현

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UserMemberController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UserMemberController.java
@@ -59,9 +59,14 @@ public class UserMemberController {
     return ResultData.newData(joinRd, "member", member);
   }
 
+  @RequestMapping("/user/member/login")
+  public String shoLogin(HttpSession httpSession) {
+    return "user/member/login";
+  }
+
   @RequestMapping("/user/member/doLogin")
   @ResponseBody
-  public ResultData doLogin(HttpSession httpSession, String loginId, String loginPw) {
+  public String doLogin(HttpSession httpSession, String loginId, String loginPw) {
     boolean isLogined = false;
 
     if (httpSession.getAttribute("loginedMemberId") != null) {
@@ -69,30 +74,30 @@ public class UserMemberController {
     }
 
     if (isLogined) {
-      return ResultData.from("F-5", "이미 로그인 되었습니다.");
+      return Ut.jsHistoryBack("이미 로그인 되었습니다.");
     }
 
     if ( Ut.empty(loginId) ) {
-      return ResultData.from("F-1", "loginId(을)를 입력해주세요.");
+      return Ut.jsHistoryBack("loginId(을)를 입력해주세요.");
     }
 
     if ( Ut.empty(loginPw) ) {
-      return ResultData.from("F-2", "loginPw(을)를 입력해주세요.");
+      return Ut.jsHistoryBack("loginPw(을)를 입력해주세요.");
     }
 
     Member member = memberService.getMemberByLoginId(loginId);
 
     if (member == null) {
-      return ResultData.from("F-3", "존재하지 않는 로그인 아이디입니다.");
+      return Ut.jsHistoryBack("존재하지 않는 로그인 아이디입니다.");
     }
 
     if (member.getLoginPw().equals(loginPw) == false) {
-      return ResultData.from("F-4", "비밀번호가 일치하지 않습니다.");
+      return Ut.jsHistoryBack("비밀번호가 일치하지 않습니다.");
     }
 
     httpSession.setAttribute("loginedMemberId", member.getId());
 
-    return ResultData.from("S-1", Ut.f("%s님 환영합니다.", member.getNickname()));
+    return Ut.jsReplace(Ut.f("%s님 환영합니다.", member.getNickname()), "/");
   }
 
   @RequestMapping("/user/member/doLogout")
