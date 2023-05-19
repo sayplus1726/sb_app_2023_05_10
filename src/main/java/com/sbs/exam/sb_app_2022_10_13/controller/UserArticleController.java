fee021ad684448fbecbfb9d680e35a73830bdfb1
sayplus1726 <sayplus1726@naver.com>
2023-05-19 10:34:08 +0900

    게시물 리스트 page 파라미터 처리

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UserArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UserArticleController.java
@@ -10,6 +10,7 @@ import com.sbs.exam.sb_app_2022_10_13.vo.Rq;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -59,7 +60,7 @@ public class UserArticleController {
   }
 
   @RequestMapping("/user/article/list")
-  public String showList(Model model, int boardId) {
+  public String showList(Model model, @RequestParam(defaultValue = "1") int boardId, @RequestParam(defaultValue = "1") int page) {
 
     Board board = boardService.getBoardById(boardId);
 
@@ -69,7 +70,9 @@ public class UserArticleController {
 
     int articlesCount = articleService.getArticlesCount(boardId);
 
-    List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId);
+    int itemsCountInAPage = 10;
+
+    List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId, itemsCountInAPage, page);
 
     model.addAttribute("board", board);
     model.addAttribute("articlesCount", articlesCount);
